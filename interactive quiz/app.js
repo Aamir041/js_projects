50abfb9c4ff2761bf0ffc3ed1d3d49245ee2627b
Aamir041 <aamirsaudagar34@gmail.com>
2022-08-14 23:01:18 -0700

    added all

--- interactive quiz/app.js
+++ interactive quiz/app.js
@@ -1,16 +1,32 @@
-const correctAnswers = ["B","A","A","B"]
-const form = document.querySelector('.quiz-form')
+const correctAnswers = ["B","A","A","B"];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector(".result");
 
 form.addEventListener('submit',e=>{
-    e.preventDefault()
-    let score = 0
-    userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value]
+    e.preventDefault();
+    let score = 0;
+    userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value];
 
     userAnswers.forEach((answers,index) => {
         if(answers === correctAnswers[index]){
-            score = score + 1;
+            score = score + 25;
         }
     });
-    console.log(score)
+    // console.log(score)
+    // result.document.querySelector("span").textContent=`${score}%`; // BIG MISTAKE :)
+    scrollTo(0,0);
+    // result.querySelector('span').textContent=`${score}%`
+    result.classList.remove("d-none");
+
+    let fscore = 0;
+    let scoreAnimator = setInterval(()=>{
+        result.querySelector('span').textContent=`${fscore}%`
+        if(fscore == score){
+            clearInterval(scoreAnimator);
+        }
+        else{
+            fscore++;
+        }
+    },15);
 })
 
